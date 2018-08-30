fdaf6b68a9645de1244b6bcb345e9dfc1bb079a1
MyznEiji <em711th@icloud.com>
2018-08-30 19:27:16 +0900

    fix selection_sort

---
+++ sort/selection_sort.py
@@ -0,0 +1,28 @@
+def selectionSort(A=[], N=0):
+    i, j, t, sw, = 0, 0, 0, 0
+    for i in range(N):
+        minj = i
+
+        # 最小値を検索
+        for j in range(i, N):
+            if(A[j] < A[minj]):
+                minj = j
+
+        ＃最小値を入れ替える
+        A[i], A[minj] = A[minj], A[i]
+
+        if(i != minj):
+            sw += 1
+    return sw
+
+# 任意の要素の配列を入力
+A = []
+N = int(input())
+for i in range(N):
+    A.append(int(input()))
+
+sw = selectionSort(A, N)
+
+
+print(A)
+print(sw)
