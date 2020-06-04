b44a80eb7b22ef16f0e8665afac1a54aad3e5997
MyznEiji <em711th@icloud.com>
2020-06-04 12:41:17 +0000

    mian test

--- jun/prime_number/main.py
+++ jun/prime_number/main.py
@@ -14,8 +14,10 @@ Non prime number
 
 
 def is_prime_v1(num: int) -> bool:
+
     if num <= 1:
         return False
+
 
     for i in range(2, num):
         if num % i == 0:
@@ -23,5 +25,7 @@ def is_prime_v1(num: int) -> bool:
     
     return True
 
+
+
 if __name__ == '__main__':
-    print(is_prime_v1(10))
+    print(is_prime_v1(11))
--- jun/prime_number/test_main.py
+++ jun/prime_number/test_main.py
@@ -1,6 +1,6 @@
 import unittest
 
-from main import is_prime_v1 as is_prime_v1
+from main import is_prime_v1 as is_prime
 
 
 class PrimeTest(unittest.TestCase):
@@ -11,7 +11,15 @@ class PrimeTest(unittest.TestCase):
 
     def test_is_prime_no(self):
         for i in [1,4,6,8,9,10,12,14,15,16,18,20]:
-            self.assertFalse(is_prime(-1))
+            self.assertFalse(is_prime(i))
+
+    def test_is_prime_negative(self):
+        self.assertFalse(is_prime(-1))
+
+    def test_is_prime_raise_typeerror(self):
+        with self.assertRaises(TypeError):
+            is_prime('string')
 
 
-
+if __name__ == '__main__':
+    unittest.main()
