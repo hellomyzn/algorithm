d4b5ed21df1295d16910da60b560245a17a8c7f4
MyznEiji <em711th@icloud.com>
2020-06-05 18:34:48 +0000

    v2.3

--- jun/number_of_character/main.py
+++ jun/number_of_character/main.py
@@ -2,6 +2,7 @@
 Input: 'This is a pen. This is an apple. Applepen.'
 Output: ('pf, 6')
 """
+from collections import Counter
 import operator
 from typing import Tuple
 
@@ -21,6 +22,30 @@ def count_chars_v1(strings: str) -> Tuple[str, int]:
     return max(l, key=operator.itemgetter(1))
 
 
+def count_chars_v2(strings: str) -> Tuple[str, int]:
+    strings = strings.lower()
+    d = {}
+
+    for char in strings:
+        if not char.isspace():
+            d[char] = d.get(char, 0) + 1
+
+    max_key = max(d, key=d.get)
+    return max_key, d[max_key]
+
+
+def count_chars_v3(strings: str) -> Tuple[str, int]:
+    strings = strings.lower()
+    d = Counter()
+
+    for char in strings:
+        if not char.isspace():
+            d[char] += 1
+
+    max_key = max(d, key=d.get)
+    return max_key, d[max_key]
+
+
 if __name__ ==  '__main__':
     s = 'This is a pen. This is an apple. Applepen.'
-    print(count_chars_v1(s))
+    print(count_chars_v3(s))
