7f46534017ae5495ba770848c221ac8c41dae79f
MyznEiji <em711th@icloud.com>
2020-06-07 06:56:48 +0000

    get_max_sequence_sum

--- jun/sum_number/main.py
+++ jun/sum_number/main.py
@@ -22,5 +22,19 @@ def get_max_sequence_sum(numbers: List[int]) -> int:
     return result_sequence
 
 
+def find_max_circular_sequence_sum(numbers: List[int]) -> int:
+    #1 [1, 2, 3, -1, -2, 1, -3, 3, 2, 1]
+    #2 [-100, 1, 2, 3, -1, -2, 1, -3, 3, 2, 1, -100]
+    max_sequence_sum = get_max_sequence_sum(numbers)
+    invert_numbers = []
+    all_sum = 0
+    for num in numbers:
+        all_sum += num
+        invert_numbers.append(-num)
+
+    max_wrap_sequence = all_sum - ( -get_max_sequence_sum(invert_numbers))
+    return max(max_sequence_sum, max_wrap_sequence)
+
+
 if __name__ == '__main__':
     print(get_max_sequence_sum([1, -2, 3, 6, -1, 2, 4, -5, 2]))
