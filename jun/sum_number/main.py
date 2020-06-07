69595a4c790fde600353ab17d9561b0530c62e7c
MyznEiji <em711th@icloud.com>
2020-06-07 07:04:21 +0000

    refactor

--- jun/sum_number/main.py
+++ jun/sum_number/main.py
@@ -12,12 +12,12 @@ from typing import List
 
 
 
-def get_max_sequence_sum(numbers: List[int]) -> int:
+def get_max_sequence_sum(numbers: List[int], operator=max) -> int:
     result_sequence, sum_sequence = 0, 0
     for num in numbers:
         # Input: [1, -2, 3, 6, -1, 2, 4, -5, 2]
-        sum_sequence = max(num, sum_sequence + num)
-        result_sequence = max(result_sequence, sum_sequence)
+        sum_sequence = operator(num, sum_sequence + num)
+        result_sequence = operator(result_sequence, sum_sequence)
 
     return result_sequence
 
@@ -26,13 +26,13 @@ def find_max_circular_sequence_sum(numbers: List[int]) -> int:
     #1 [1, 2, 3, -1, -2, 1, -3, 3, 2, 1]
     #2 [-100, 1, 2, 3, -1, -2, 1, -3, 3, 2, 1, -100]
     max_sequence_sum = get_max_sequence_sum(numbers)
-    invert_numbers = []
-    all_sum = 0
-    for num in numbers:
-        all_sum += num
-        invert_numbers.append(-num)
+    # invert_numbers = []
+    # all_sum = 0
+    # for num in numbers:
+    #     all_sum += num
+    #     invert_numbers.append(-num)
 
-    max_wrap_sequence = all_sum - ( -get_max_sequence_sum(invert_numbers))
+    max_wrap_sequence = all_sum - (-get_max_sequence_sum(invert_numbers))
     return max(max_sequence_sum, max_wrap_sequence)
 
 
