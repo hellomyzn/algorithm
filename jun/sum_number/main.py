9469a0bf11489c4922d114c17dc84f1b3a9f2310
MyznEiji <em711th@icloud.com>
2020-06-07 06:44:52 +0000

    refacter

--- jun/sum_number/main.py
+++ jun/sum_number/main.py
@@ -16,17 +16,7 @@ def get_max_sequence_sum(numbers: List[int]) -> int:
     result_sequence, sum_sequence = 0, 0
     for num in numbers:
         # Input: [1, -2, 3, 6, -1, 2, 4, -5, 2]
-        # temp_sum_sequence = sum_sequence + num
-        # if num < temp_sum_sequence:
-        #     sum_sequence = temp_sum_sequence
-        # else:
-        #     sum_sequence = num
-
         sum_sequence = max(num, sum_sequence + num)
-
-        # if result_sequence < sum_sequence:
-        #     result_sequence = sum_sequence
-
         result_sequence = max(result_sequence, sum_sequence)
 
     return result_sequence
