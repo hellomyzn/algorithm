6af7f1bf222cfecbfe6508909813b4484cce441e
MyznEiji <em711th@icloud.com>
2020-06-08 09:50:52 +0000

    v2

--- jun/remove_duplication/main.py
+++ jun/remove_duplication/main.py
@@ -18,7 +18,17 @@ def delete_duplicate_v1(numbers: List[int]) -> None:
     numbers[:] = tmp
 
 
+def delete_duplicate_v2(numbers: List[int]) -> None:
+    tmp = [numbers[0]]
+    i, len_num = 0, len(numbers) - 1
+    while i < len_num:
+        if numbers[i] != numbers[i+1]:
+            tmp.append(numbers[i+1])
+        i += 1
+    numbers[:] = tmp
+
+
 if __name__ == "__main__":
     l = [1,3,3,5,5,7,7,7,10,12,12,15]
-    delete_duplicate_v1(l)
+    delete_duplicate_v2(l)
     print(l)
