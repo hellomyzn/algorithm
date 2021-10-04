9b26f5180bb42b26350ddb6946b75c967d32d673
hellomyzn <em711th@icloud.com>
2021-10-04 18:47:36 +0900

    bubble sort

--- backend/sort/bubble_sort.py
+++ backend/sort/bubble_sort.py
@@ -1,3 +1,6 @@
+import random
+
+
 """
 flagを用いたバブルソート
 """
@@ -26,13 +29,33 @@ def bubbleSort(A=None, N=1):
     return sw
 
 
+# from Jun Sakai lesson
+def bubble_sort(numbers: list[int]) -> list[int]:
+    len_numbers = len(numbers)
+    for i in range(len_numbers):
+        for j in range(len_numbers -1 -i):
+            if numbers[j] > numbers[j+1]:
+                numbers[j+1], numbers[j] = numbers[j], numbers[j+1]
+    return numbers
+
+
+
 if __name__ == "__main__":
 
-    A = []
-    N = int(input("\n数列の長さを入力してください\n"))
-    line = "===========================\n"
-    for i in range(N):
-        A.append(int(input(i, "個目の要素を入力してください\n")))
-    print(line, A,"\n", line)
-    sw = bubbleSort(A, N)
+    # OLD CODES...
+
+    # A = []
+    # N = int(input("\n数列の長さを入力してください\n"))
+    # line = "===========================\n"
+    # for i in range(N):
+    #     A.append(int(input(i, "個目の要素を入力してください\n")))
+    # print(line, A,"\n", line)
+    # sw = bubbleSort(A, N)
+
+
+    # FROM JUN SAKAI LESSON
+    nums = [random.randint(0,1000) for _ in range(10)] 
+    print(bubble_sort(nums))
+
+
 
