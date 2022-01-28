3d20fd4f6eac987ac2ad14cff6ecbb7cb44719db
hellomyzn <em711th@icloud.com>
2022-01-28 10:58:20 +0900

    reverse

--- backend/linked_list/singly-linked_list.py
+++ backend/linked_list/singly-linked_list.py
@@ -104,6 +104,94 @@ class LinkedList(object):
         previous_node.next = current_node.next
         current_node = None
 
+
+    def reverse_iterative(self) -> None:
+        """    
+            l.append(1): new_node =  Node(1)
+                         self.head = Node(1)
+            l.append(2): new_node =  Node(2)
+                         last_node = Node(1)
+                         Node(1).next = Node(2)
+            l.append(3): new_node =  Node(3)
+                         last_node = Node(2)
+                         Node(2).next = Node(3)
+            ...
+            l.reverse_iterative(): current_node = Node(1)
+                                    while Node(1)
+                                        next_node = Node(2)
+                                        Node(1).next = None
+                                        previous_node = Node(1)
+                                        current_node = Node(2)
+                                    while Node(2)
+                                        next_node = Node(3)
+                                        Node(2).next = Node(1)
+                                        previous_node = Node(2)
+                                        current_node = Node(3)
+                                    while Node(3)
+                                        next_node = None
+                                        Node(3).next = Node(2)
+                                        previous_node = Node(3)
+                                        current_node = None
+                                    self.head = Node(3)
+                         
+        """
+        previous_node = None
+        current_node = self.head
+        while current_node:
+            next_node = current_node.next
+            current_node.next = previous_node
+            previous_node = current_node
+            current_node = next_node
+
+        self.head = previous_node
+
+
+    def reverse_recursive(self) -> None:
+        """    
+            l.append(1): new_node =  Node(1)
+                         self.head = Node(1)
+            l.append(2): new_node =  Node(2)
+                         last_node = Node(1)
+                         Node(1).next = Node(2)
+            l.append(3): new_node =  Node(3)
+                         last_node = Node(2)
+                         Node(2).next = Node(3)
+            ...
+            l.reverse_recursive():
+                _reverse_recursive(Node(1), None):
+                    next_node = Node(2)
+                    Node(1).next = None
+                    previous_node = Node(1)
+                    current_node = Node(2)
+
+                    _reverse_recursive(Node(2), Node(1)):
+                        next_node = Node(3)
+                        Node(2).next = Node(1)
+                        previous_node = Node(2)
+                        current_node = Node(3)
+
+                        _reverse_recursive(Node(3), Node(2)):
+                            next_node = None
+                            Node(3).next = Node(2)
+                            previous_node = Node(3)
+                            current_node = None
+
+                            _reverse_recursive(None, Node(3)):
+                self.head = Node(3)
+                         
+        """
+        def _reverse_recursive(current_node: Node, previous_node: Node):
+            if not current_node:
+                return previous_node
+            
+            next_node = current_node.next
+            current_node.next = previous_node
+            previous_node = current_node
+            current_node = next_node
+            return _reverse_recursive(current_node, previous_node)
+
+        self.head = _reverse_recursive(self.head, None)
+
 if __name__ == '__main__':
     l = LinkedList()
     l.append(1)
@@ -111,6 +199,13 @@ if __name__ == '__main__':
     l.append(3)
     l.insert(9)
     l.print()
-    l.remove(3)
+    l.remove(2)
     print('#########')
     l.print()
+    l.reverse_iterative()
+    print('#########')
+    l.print()
+    l.reverse_recursive()
+    print('#########')
+    l.print()
+
