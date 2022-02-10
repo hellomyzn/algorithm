6478210918cb105568106870e634d8910d40cc6e
hellomyzn <em711th@icloud.com>
2022-02-10 12:30:21 +0900

    primes

---
+++ backend/quiz/generate_primes.py
@@ -0,0 +1,70 @@
+
+"""
+Input: 50 => Output: [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47]
+"""
+
+from typing import List, Generator
+
+def generate_primes_v1(number: int) -> List[int]:
+    primes = []
+    i = 0
+    for x in range(2, number + 1):
+        for y in range(2, x):
+            i += 1
+            if x % y == 0:
+                break
+        else:
+            i += 1
+            primes.append(x)
+    print('v1 = ', i)
+    return primes
+
+def generate_primes_v2(number: int) -> list[int]:
+    primes = []
+    cache = {}
+    i = 0
+    for x in range(2, number + 1):
+        
+        is_prime = cache.get(x)
+        if is_prime is False:
+            continue
+        
+        primes.append(x)
+        cache[x] = True
+        for y in range(x*2, number+1, x):
+            i += 1
+            cache[y] = False
+    print('v2 = ', i)
+    return primes
+
+def generate_primes_v3(number: int) -> Generator[int, None, None]:
+    cache = {}
+    i = 0
+    for x in range(2, number +1):
+        is_prime = cache.get(x)
+        if is_prime is False:
+            continue
+        yield x
+        cache[x] = True
+        for y in range(x*2, number+1, x):
+            i += 1
+            cache[y] = False
+
+    print('v3 = ', i)
+
+if __name__ == '__main__':
+    n = 1000
+    import time
+
+    start = time.time()
+    print(generate_primes_v1(n))
+    print(time.time() - start)
+
+    start = time.time()
+    print(generate_primes_v2(n))
+    print(time.time() - start)
+
+    start = time.time()
+    print([i for i in generate_primes_v3(n)])
+    print(time.time() - start)
+    
