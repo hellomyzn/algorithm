ed141746d154a53b152f1fb1d86da51addbdccc7
hellomyzn <em711th@icloud.com>
2022-05-30 11:36:59 +0900

    add acc

---
+++ backend/atcoder/abc/253/a/main.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+def main():
+    a, b, c = map(int, input().split())
+    median = a + b + c - max(a, b, c) - min(a, b, c)
+    if median == b:
+        print("yes")
+    else:
+        print("No")
+
+if __name__ == "__main__":
+    main()
+    
