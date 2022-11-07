6b188a006c7d0bbd2593e08daeef14bc1f70d088
hellomyzn <em711th@icloud.com>
2022-11-08 08:01:25 +1100

    a06

--- backend/tessoku/chapter02/a06.py
+++ backend/tessoku/chapter02/a06.py
@@ -1,16 +1,14 @@
 N, Q = map(int, input().split())
 A = list(map(int, input().split()))
-L = []
-for q in range(Q):
-    l, r = map(int, input().split())
-    L.append([l, r])
+L = [ None ] * Q
+R = [ None ] * Q
+for i in range(Q):
+    L[i], R[i] = map(int, input().split())
 
-ruiseki = []
-temp = 0
-for i,v in enumerate(A):
-    temp += v
-    ruiseki.append(temp)
+S = [ None ] * (N + 1)
+S[0] = 0
+for i in range(N):
+    S[i + 1] = S[i] + A[i]
 
-for l in L:
-    s, u = l[0], l[1]
-    turnout = ruiseki[]
+for i in range(Q):
+    print(S[R[i]] - S[L[i] - 1])
