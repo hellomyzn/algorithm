ee7a3c8b17b1a43f76efeca6f528c54d29a56968
hellomyzn <em711th@icloud.com>
2022-11-22 09:05:52 +1100

    a14

--- backend/tessoku/chapter03/a14.py
+++ backend/tessoku/chapter03/a14.py
@@ -1,5 +1,36 @@
+import bisect
+import sys
+
 N, K = map(int, input().split())
-A = list(map(int, input.split()))
-B = list(map(int, input.split()))
-C = list(map(int, input.split()))
-D = list(map(int, input.split()))
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+C = list(map(int, input().split()))
+D = list(map(int, input().split()))
+
+P = [] * (len(A) * len(B))
+Q = [] * (len(C) * len(D))
+
+for i in range(N):
+    for j in range(N):
+        P.append(A[i] + B[j])
+
+for i in range(N):
+    for j in range(N):
+        Q.append(C[i] + D[j])
+
+# 配列 Q を小さい順にソート
+Q.sort()
+
+# for i in Q:
+#     for j in P:
+#         if K - i == j:
+#             print('Yes')
+
+# 二分探索
+for i in range(len(P)):
+    pos1 = bisect.bisect_left(Q, K-P[i])
+    if pos1<N*N and Q[pos1]==K-P[i]:
+        print("Yes")
+        sys.exit(0)
+
+print("No")
