0074a2b129de67bf0627ccdde5833124da73e394
hellomyzn <em711th@icloud.com>
2022-12-17 11:36:03 +1100

    update: a2

--- backend/tessoku/chapter01/a02.py
+++ backend/tessoku/chapter01/a02.py
@@ -1,12 +1,12 @@
 N, X = map(int, input().split())
 A = list(map(int, input().split()))
-answer = False
+found = False
 
 for i in range(N):
     if A[i] == X:
-        answer = True
+        found = True
 
-if answer == True:
+if found == True:
     print("Yes")
 else:
     print("No")
