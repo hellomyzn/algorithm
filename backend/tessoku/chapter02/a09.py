fde4c30ff2b28018c1cdb6a5ee958d874953770b
hellomyzn <em711th@icloud.com>
2023-01-06 08:48:04 +1100

    fix 09

--- backend/tessoku/chapter02/a09.py
+++ backend/tessoku/chapter02/a09.py
@@ -1,31 +1,32 @@
 H, W, N = map(int, input().split())
-A = [ None ] * N
-B = [ None ] * N
-C = [ None ] * N
-D = [ None ] * N
+X1 = [ None ] * N
+Y1 = [ None ] * N
+X2 = [ None ] * N
+Y2 = [ None ] * N
 
 for i in range(N):
-    A[i], B[i], C[i], D[i] = map(int, input().split())
+    X1[i], Y1[i], X2[i], Y2[i], = map(int, input().split())
 
-X = [ [ 0 ] * (W+2) for i in range(H+2)]
-Z = [ [ 0 ] * (W+2) for i in range(H+2)]
+diff = [[ 0 ] * (W*2) for _ in range(H+2)]
+cumulative_sum = [ [ 0 ] * (W*2) for _ in range(H+2)]
 
 for i in range(N):
-    X[A[i]][B[i]] += 1
-    X[C[i]+1][D[i]+1] += 1
-    X[A[i]][D[i]+1] -= 1
-    X[C[i]+1][B[i]] -= 1
+    diff[X1[i]][Y1[i]] += 1
+    diff[X2[i] + 1][Y2[i] + 1] += 1
+    diff[X2[i] + 1][Y1[i]] -= 1
+    diff[X1[i]][Y2[i] + 1] -= 1
 
-for i in range(1, H+1):
-    for j in range(1, W+1):
-        Z[i][j] = Z[i][j-1] + X[i][j]
-for j in range(1, W+1):
-    for i in range(1, H+1):
-        Z[i][j] = Z[i-1][j] + Z[i][j]
+# ->
+for h in range(1, H+1):
+    for w in range(1, W+1):
+        cumulative_sum[h][w] = cumulative_sum[h][w-1] + diff[h][w]
 
-for i in range(1, H+1):
-    for j in range(1, W+1):
-        if j >= 2:
-            print(" ", end="")
-        print(Z[i][j],end="")
+# V
+for w in range(1, W+1):
+    for h in range(1, H+1):
+        cumulative_sum[h][w] = cumulative_sum[h-1][w] + cumulative_sum[h][w]
+
+for h in range(1, H+1):
+    for w in range(1, W+1):
+        print(cumulative_sum[h][w], end=" ")
     print("")
