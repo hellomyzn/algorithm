0cfea8a80a2306f90e7c363fd5adaf8580f34f48
hellomyzn <em711th@icloud.com>
2023-01-07 09:37:11 +1100

    fix: binary search

--- backend/tessoku/chapter03/a11.py
+++ backend/tessoku/chapter03/a11.py
@@ -1,18 +1,19 @@
-def search(x, A):
-    L = 0
-    R = N - 1
-    while L <= R:
-        M = (L + R) // 2
-        if x < A[M]:
-            R = M -1
-        if x == A[M]:
-            return M
-        if x > A[M]:
-            L = M + 1
+from typing import List
+
+def binary_search(value: int, numbers: List[int]) -> int:
+    l, r  = 0, len(numbers) -1
+    while l <= r:
+        mid = (l + r) // 2
+        if numbers[mid] == value:
+            return mid
+        elif numbers[mid] < value:
+            l = mid + 1
+        else:
+            r = mid -1
     return -1
 
 N, X = map(int, input().split())
 A = list(map(int, input().split()))
 
-Answer = search(X, A)
+Answer = binary_search(X, A)
 print(Answer + 1)
